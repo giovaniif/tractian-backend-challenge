8913df4eb3867bda2baea43185f04204feb6de1a
Giovani Farias <riccog.25@gmail.com>
2021-09-12 19:54:21 -0300

    refactor: change test strategy to use jest-mock-extended

--- tests/data/services/create-company.spec.ts
+++ tests/data/services/create-company.spec.ts
@@ -1,22 +1,16 @@
-import { CompanyRepository } from "@/data/contracts/repos"
+import { mock } from 'jest-mock-extended'
+
+import { CompanyRepository } from '@/data/contracts/repos'
 import { CreateCompanyService } from '@/data/services'
-
-class CompanyRepositorySpy implements CompanyRepository {
-  companyName?: string
-  
-  async create (params: CompanyRepository.Params): Promise<CompanyRepository.Result> {
-    this.companyName = params.companyName
-  }
-
-}
 
 describe('Create Company Service', () => {
   it('should call create company with correct params', async () => {
-    const companyRepo = new CompanyRepositorySpy()
+    const companyRepo = mock<CompanyRepository>()
     const sut = new CreateCompanyService(companyRepo)
 
     await sut.perform({ companyName: 'any_name' })
 
-    expect(companyRepo.companyName).toBe('any_name')
+    expect(companyRepo.create).toHaveBeenCalledWith({ companyName: 'any_name' })
+    expect(companyRepo.create).toHaveBeenCalledTimes(1)
   })
 })
