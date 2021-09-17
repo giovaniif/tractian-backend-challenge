90bd1dd73a6106567277c6a13a58ed7ed92c4f23
Giovani Farias <riccog.25@gmail.com>
2021-09-16 21:46:39 -0300

    test: ensure ValidationComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -4,11 +4,14 @@ interface Validator {
   validate: () => Error | undefined
 }
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor(private readonly validators: Validator[]) {}
   
-  validate (): undefined {
-    return undefined
+  validate (): undefined | Error {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) return error
+    }
   }
 }
 
@@ -34,5 +37,14 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValueOnce(new Error('error_1'))
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
+    
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
