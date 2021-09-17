3e3fb919ec9bf47dc094a3ff22b0d8b791424b3c
Giovani Farias <riccog.25@gmail.com>
2021-09-17 00:40:50 -0300

    feat: add 204 return to express adapter

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -5,6 +5,9 @@ import { Controller } from '@/application/controllers'
 export const adaptExpressRoute = (controller: Controller): RequestHandler => {
   return async (req: Request, res: Response) => {
     const { statusCode, data } = await controller.handle({ ...req.body, ...req.params })
+
+    if (statusCode === 204) res.status(204).send()
+
     const json = statusCode === 200 ? data : { error: data.message }
 
     res.status(statusCode).json(json)
