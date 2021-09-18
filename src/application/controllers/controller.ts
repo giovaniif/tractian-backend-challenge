298216df8f79e84dbaa0c8db1cfa817ca8647b4d
Giovani Farias <riccog.25@gmail.com>
2021-09-18 17:04:22 -0300

    fix: export user entity

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -15,6 +15,7 @@ export abstract class Controller {
     try {
       return await this.perform(httpRequest)
     } catch (err: any) {
+      console.log(err)
       return serverError(err)
     }
   }
