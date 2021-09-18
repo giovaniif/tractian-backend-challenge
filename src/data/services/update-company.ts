aaf079d13d8d597e850170516e884d774e14441e
Giovani Farias <riccog.25@gmail.com>
2021-09-17 21:22:24 -0300

    test: ensure update company controller calls load with company name

--- src/data/services/update-company.ts
+++ src/data/services/update-company.ts
@@ -5,7 +5,9 @@ import { UpdateCompany } from '@/domain/features/update-company'
 export class UpdateCompanyService implements UpdateCompany {
   constructor(private readonly companyRepo: LoadCompanyRepository & LoadCompanyByIdRepository) {}
 
-  async perform(params: UpdateCompany.Params): Promise<any> {
-    if (params.companyName.length < 2) return new InvalidNameError()
+  async perform({ companyName }: UpdateCompany.Params): Promise<any> {
+    if (companyName.length < 2) return new InvalidNameError()
+
+    await this.companyRepo.load({ companyName })
   }
 }
--- tests/data/services/update-company.spec.ts
+++ tests/data/services/update-company.spec.ts
@@ -7,10 +7,12 @@ import { InvalidNameError } from '@/domain/errors'
 describe('Create Company Service', () => {
   let sut: UpdateCompanyService
   let companyName: string
+  let companyId: string
   let companyRepo: MockProxy<LoadCompanyByIdRepository & LoadCompanyRepository>
 
   beforeAll(() => {
     companyName = 'any_name'
+    companyId = 'any_id'
     companyRepo = mock()
     companyRepo.load.mockResolvedValue(undefined)
   })
@@ -20,15 +22,15 @@ describe('Create Company Service', () => {
   })
 
   it('should return an InvalidNameError if the name length is smaller than 2', async () => {
-    const error = await sut.perform({ companyName: 'a', companyId: 'any_id' })
+    const error = await sut.perform({ companyName: 'a', companyId })
     
     expect(error).toEqual(new InvalidNameError())
   })
 
-  // it('should call load company with correct params', async () => {
-  //   await sut.perform({ companyName })
+  it('should call load company with correct params', async () => {
+    await sut.perform({ companyName, companyId })
 
-  //   expect(companyRepo.load).toHaveBeenCalledWith({ companyName })
-  //   expect(companyRepo.load).toHaveBeenCalledTimes(1)
-  // })
+    expect(companyRepo.load).toHaveBeenCalledWith({ companyName })
+    expect(companyRepo.load).toHaveBeenCalledTimes(1)
+  })
 })
